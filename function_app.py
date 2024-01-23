9edccca6723429b6cffd8d28eeeeb88d461fd817
Reese Dykman <reese.dykman@symend.com>
2024-01-23 15:26:51 -0700

    testing variables

--- function_app.py
+++ function_app.py
@@ -1,25 +1,28 @@
 import azure.functions as func
 import logging
+import os
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="http_trigger1", auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger1(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Trigger 1 Processing Request')
 
-    name = req.params.get('name')
-    if not name:
+    country = req.params.get('country')
+    city = req.parmas.get("city")
+    if not country or not city:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            city = req_body.get('city')
+            country = req_body.get('country')
 
-    if name:
+    if country and city:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             f"{os.environ('WEATHER_KEY')}",
              status_code=200
         )
