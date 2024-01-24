c4d0316e124c0a8d4c703007431507739935074d
Reese Dykman <reese.dykman@symend.com>
2024-01-24 13:44:23 -0700

    testing calling other api

--- weather.py
+++ weather.py
@@ -10,4 +10,5 @@ class Weather:
     def _getWeather(self):
         URL = f"http://api.weatherstack.com/current?access_key={os.environ.get('WEATHER_KEY')}&query={self.city},{self.country}"
         response = requests.get(URL)
-        return json.loads(response.current)
+        data = json.loads(response)
+        return data
